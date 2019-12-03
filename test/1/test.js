27e4396cedec7dacce24d863e82a4a1623409d04
Elyes <46033027+Elyesc4@users.noreply.github.com>
2019-12-03 22:16:44 +0100

    new
    testing nad fixing

---
+++ test/1/test.js
@@ -0,0 +1,11 @@
+
+const speed = 10;
+
+const moveBackground = () => {
+    let mouseXPos = (event.x / window.innerWidth) * 100;
+    let mouseYPos = (event.y / window.innerHeight) * 100;
+
+    document.getElementById('text').style.backgroundPosition = `${mouseXPos / speed}%${mouseYPos / speed}%`;
+  }
+
+document.getElementById('wrapper').addEventListener('mousemove', moveBackground);
